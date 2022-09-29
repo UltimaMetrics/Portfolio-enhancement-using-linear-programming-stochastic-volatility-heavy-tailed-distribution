03228a04e73f3e74ccb07d8e9fbf978776acfe43
UltimaMetrics <sigma.vantage@gmail.com>
2022-09-29 15:37:00 -0400

    Delete RegBeta.R

--- RegBeta.R
+++
@@ -1,8 +0,0 @@
-
-
-library(readxl)
-
-RegBeta <- read_excel("D:/Stony Brook AMS/AMS 2021 Fall/AMS 518 Stochastic and Optimization/AMS518 project/AMS518 Final Project submission_Chi-Sheng Lo/TW50 index tracking project/RegBeta.xlsx")
-
-ModelReg=lm(TWA50 ~ ., data = RegBeta)
-ModelReg
